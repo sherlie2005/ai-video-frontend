04cf4383db940791271cdc13b8d9cec60f561ede
Sherlie Angel <sherlieangelballikuri@gmail.com>
2025-05-02 22:42:49 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import TextInputPage from "./pages/TextInputPage";
+import VideoDisplayPage from "./pages/VideoDisplayPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/input" element={<TextInputPage />} />
+        <Route path="/video" element={<VideoDisplayPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
